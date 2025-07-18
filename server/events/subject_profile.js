66fc5a16d3862ab142c2a209c06115b0eb635f61
Mohamed Amr <mohamedamr@Emperors-MacBook-Pro.local>
2025-07-19 02:26:10 +0300

    calculate tutor rating is finished

--- server/events/subject_profile.js
+++ server/events/subject_profile.js
@@ -5,11 +5,18 @@ import {
     SubjectProfile
 } from '../models/subject.js'
 
-export async function calculateSubjectProfileRating(subject_profile_id){
+export async function calculateSubjectProfileRating(subject_profile_id, rate, isNew){
     try{
         const MatchSubjectProfile = await SubjectProfile.findById(subject_profile_id)
         if (!MatchSubjectProfile) {console.error({invalid: "subject profile not found"})}
-        //MatchSubjectProfile.rating = await calculateRating(MatchSubjectProfile)
+        const n = await Review.countDocuments()
+        if (isNew){
+            MatchSubjectProfile.rating = ((MatchSubjectProfile.rating * (n-1)) + rate) / n
+        }else{
+            MatchSubjectProfile.rating = ((MatchSubjectProfile.rating * (n+1)) - rate) / n
+        }
+        await MatchSubjectProfile.save()
+        console.log({message: "subject profile rating updated", subject_profile_id, rating: MatchSubjectProfile.rating})
     }catch(err){
     console.error({message: "error in validating subject profile", err})
   }    
