4f7ed921e829ec7f640365f803290248de7ec2ab
Mohamed Amr <mohamedamr@Emperors-MacBook-Pro.local>
2025-07-19 03:41:23 +0300

    enrollment system has finished

--- server/events/subject_profile.js
+++ server/events/subject_profile.js
@@ -19,5 +19,5 @@ export async function calculateSubjectProfileRating(subject_profile_id, rate, is
         console.log({message: "subject profile rating updated", subject_profile_id, rating: MatchSubjectProfile.rating})
     }catch(err){
     console.error({message: "error in validating subject profile", err})
-  }    
+  }
 }
